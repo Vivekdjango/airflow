eea19f62df1792974e895b6c500cf66e39814f29
Vivek Kumar Sinha <vivek.strokes@gmail.com>
2023-08-07 07:11:59 +0530

    Update python_dag.py

--- python_dag.py
+++ python_dag.py
@@ -10,6 +10,6 @@ def my_func(*op_args):
 dag = DAG('python_dag', description='Simple tutorial DAG',
           schedule_interval='0 12 * * *',
           start_date=datetime(2019, 7, 12), catchup=False)
-dummy_task      = DummyOperator(task_id='dummy_task', retries=3)
-python_task     = PythonOperator(task_id='python_task', python_callable=my_func, op_args=['one', 'two', 'three'])
+dummy_task      = DummyOperator(task_id='dummy_task', retries=3, dag=dag)
+python_task     = PythonOperator(task_id='python_task', python_callable=my_func, op_args=['one', 'two', 'three'], dag=dag)
 dummy_task >> python_task
